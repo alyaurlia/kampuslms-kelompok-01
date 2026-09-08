5e789b3a5b79e59225bdb0456aa9c6037994ddda
AdeliaIsraE <10241004@student.itk.ac.id>
2026-09-09 00:33:24 +0800

    jujur aku lupa apa yang ku modified

--- app/Http/Controllers/Coursecontroller.php
+++ app/Http/Controllers/Coursecontroller.php
@@ -75,10 +75,4 @@ class CourseController extends Controller
  
         return view('mata-kuliah.show', compact('mataKuliah'));
     }
-
-    public function tentang()
-    {
-        return view('tentang');
-    }
-    
 }
